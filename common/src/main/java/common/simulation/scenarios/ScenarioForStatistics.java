ee7fce81fe138810fc30e148ec97e73f804ee3db
hahnjas <hahnjasper@gmail.com>
2014-05-19 11:28:13 +0200

    Started Statistics class.

---
+++ common/src/main/java/common/simulation/scenarios/ScenarioForStatistics.java
@@ -0,0 +1,52 @@
+package common.simulation.scenarios;
+
+import se.sics.kompics.p2p.experiment.dsl.SimulationScenario;
+
+@SuppressWarnings("serial")
+public class ScenarioForStatistics extends Scenario {
+	private static SimulationScenario scenario = new SimulationScenario() {{
+                
+		StochasticProcess process0 = new StochasticProcess() {{
+			eventInterArrivalTime(constant(1000));
+			// Shatha modification - it was 3
+			raise(60, Operations.peerJoin(), 
+                                uniform(0, Integer.MAX_VALUE), 
+                                constant(8), constant(12000)
+                             );
+		}};
+                
+		StochasticProcess process1 = new StochasticProcess() {{
+			eventInterArrivalTime(constant(100));
+			// Shatha modification, it was 100
+			raise(3000, Operations.requestResources(), 
+                                uniform(0, Integer.MAX_VALUE),
+                                constant(2), constant(2000),
+                                constant(1000*60*1) // 1 minute
+                                );
+		}};
+	
+                
+                // TODO - not used yet
+		StochasticProcess failPeersProcess = new StochasticProcess() {{
+			eventInterArrivalTime(constant(100));
+			raise(1, Operations.peerFail, 
+                                uniform(0, Integer.MAX_VALUE));
+		}};
+                
+		StochasticProcess terminateProcess = new StochasticProcess() {{
+			eventInterArrivalTime(constant(100));
+			raise(1, Operations.terminate);
+		}};
+		process0.start();
+		process1.startAfterTerminationOf(2000, process0);
+		failPeersProcess.startAfterTerminationOf(4000, process1);
+		// 
+		terminateProcess.startAfterTerminationOf(1000, failPeersProcess);
+               // terminateProcess.startAfterTerminationOf(100*1000, process1);
+	}};
+
+	// -------------------------------------------------------------------
+	public ScenarioForStatistics() {
+		super(scenario);
+	}
+}
