4125b38d86af5ec05b3fb703b03a348afcdbf5fd
shatha2014 <shatha@kth.se>
2014-05-27 16:08:16 +0200

    change

--- resourcemanager/src/main/java/resourcemanager/system/peer/rm/AllocateResourcesTimeout.java
+++ resourcemanager/src/main/java/resourcemanager/system/peer/rm/AllocateResourcesTimeout.java
@@ -1,13 +1,19 @@
 package resourcemanager.system.peer.rm;
 
+import se.sics.kompics.address.Address;
 import se.sics.kompics.timer.ScheduleTimeout;
 import se.sics.kompics.timer.Timeout;
 
 public class AllocateResourcesTimeout extends Timeout {
 
+	private final Address peer;
 
-	public AllocateResourcesTimeout(ScheduleTimeout request) {
+	public AllocateResourcesTimeout(ScheduleTimeout request, Address peer) {
 		super(request);
+		this.peer = peer;
 	}
 
+	public Address getPeer() {
+		return peer;
+	}
 }
